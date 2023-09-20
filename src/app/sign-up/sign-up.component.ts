ec846e81e3aff12c9a847c36a90cad4941b50fe6
KevinDavoust <kevin.davoust@orange.fr>
2023-09-20 11:46:59 +0200

    challenge

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent {
+
+  model: User = new User("", "", "", "");
+
+  onSubmit(): void {
+    console.log(this.model);
+  }
 
 }
