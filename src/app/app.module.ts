76da33b165ab41b17ff941dec86ad81458cb60cb
KevinDavoust <kevin.davoust@orange.fr>
2023-09-20 10:02:45 +0200

    quete4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { ROUTES } from "./app.routes";
 
 import { AppComponent } from "./app.component";
 import { BlockComponent } from "./block/block.component";
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { FormsModule } from "@angular/forms";
 import { DirectivesComponent } from './directives/directives.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [AppComponent,
-  BlockComponent,
-  UserProfileComponent,
-  DirectivesComponent],
-  imports: [BrowserModule,
-  FormsModule],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule {}
+    BlockComponent,
+    UserProfileComponent,
+    DirectivesComponent,
+    MenuComponent,
+    SignUpComponent,],
+    
+    imports: [BrowserModule,
+      FormsModule,
+      RouterModule.forRoot(ROUTES)
+    ],
+      providers: [],
+      bootstrap: [AppComponent]
+    })
+    export class AppModule {}
